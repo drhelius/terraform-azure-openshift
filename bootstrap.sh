8a0db4ed10518be25e682e2ebfe7b8f40b5cc618
Ignacio Sanchez Gines <nacho@geardome.com>
2017-09-20 10:07:15 +0200

    Bastion verification removed

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,21 +26,6 @@ echo "--> $ADMIN_USER"
 
 cd ..
 
-echo "Attempting to connect to bastion server..."
-index=1
-maxConnectionAttempts=10
-sleepSeconds=5
-while (( $index <= $maxConnectionAttempts ))
-do
-  ssh -o StrictHostKeychecking=no -i certs/bastion.key $ADMIN_USER@$BASTION_IP pwd >/dev/null
-  case $? in
-    (0) echo "Success!!"; break ;;
-    (*) echo "${index} of ${maxConnectionAttempts}> Bastion SSH server not ready yet, waiting ${sleepSeconds} seconds..." ;;
-  esac
-  sleep $sleepSeconds
-  (( index+=1 ))
-done
-
 echo "Transfering private key to bastion server..."
 scp -o StrictHostKeychecking=no -i certs/bastion.key certs/openshift.key $ADMIN_USER@$BASTION_IP:/home/openshift/.ssh/id_rsa
 
