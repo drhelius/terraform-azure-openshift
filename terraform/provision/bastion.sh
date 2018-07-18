5c6ea855240367165a5b20475ad6ce3be2baec4d
Ignacio Sanchez Gines <nacho@geardome.com>
2018-07-18 14:28:55 +0200

    fix missing dependency java-1.8.0-openjdk-headless

--- terraform/provision/bastion.sh
+++ terraform/provision/bastion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo yum -y install epel-release
 sudo yum -y update
-sudo yum -y install ansible pyOpenSSL wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct python-passlib httpd-tools tmux vim
+sudo yum -y install ansible pyOpenSSL wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct python-passlib httpd-tools java-1.8.0-openjdk-headless tmux vim
